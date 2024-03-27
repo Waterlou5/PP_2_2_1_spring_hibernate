1c246b0b7e33c4bcde7c3a8af82b9eaa0d4da845
Victor Rachev <waterlou5@mail.ru>
2024-03-27 11:12:35 +0300

    вроде все работает

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -12,33 +12,29 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
-   @Override
-   public User getUserByCar(String model, int series) {
-      String hql = "SELECT u FROM User u WHERE u.car.model = :model AND u.car.series = :series";
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
-      query.setParameter("model", model);
-      query.setParameter("series", series);
-      List<User> resultList = query.getResultList();
-      return resultList.get(0);
-   }
+    @Override
+    public User getUserByCar(String model, int series) {
+        String hql = "SELECT u FROM User u WHERE u.car.model = :model AND u.car.series = :series";
+        return sessionFactory.getCurrentSession().createQuery(hql, User.class).setParameter("model", model).setParameter("series", series).uniqueResult();
+    }
 
-   @Override
-   public void addCar(User user, Car car) {
-      sessionFactory.getCurrentSession().save(car);
-   }
+    @Override
+    public void addCar(User user, Car car) {
+        sessionFactory.getCurrentSession().save(car);
+    }
 }
